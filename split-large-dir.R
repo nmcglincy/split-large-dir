224104c6e385d0b1a5433a26cb140d23773c1e92
Nicholas McGlincy <nmcglincy@gmail.com>
2015-08-06 09:11:24 -0700

    work flow schematic

---
+++ split-large-dir.R
@@ -0,0 +1,13 @@
+# initiate counter
+# 
+# system call - make folder based on the counter
+# 
+# make a list of the files in the directory, not including the folders
+# 
+# system call - move the first 100 files on the file list into the folder
+# 
+# counter + 1
+# 
+# loop back to beginning
+# 
+# system call - zip all the directories in the top dir
