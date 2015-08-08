05afaaf92df5ff0a507c9a943bf120a877b6b042
Nicholas McGlincy <nmcglincy@gmail.com>
2015-08-08 11:16:41 -0700

    Adding loop to deal with leftovers

--- split-large-dir.R
+++ split-large-dir.R
@@ -1,22 +1,29 @@
+# This script splits a directory into a number of subdirectories.
+# No spaces in file names please.
+# 
 # initiate counter
-while (length(list.files(include.dirs = FALSE)) > 0) {
-  #
-  a = 1
+a = 1
+while (length(list.files(include.dirs = FALSE)) > 100) {
+  # 
   # system call - make folder based on the counter
-  # 
   system(paste("mkdir ", "russ-pics-", a, sep = ""))
+  # 
   # make a list of the files in the directory, not including the folders
-  # 
   files = list.files(include.dirs = FALSE)
-  # system call - move the first 100 files on the file list into the folder
   # 
+  # system call - move the first 100 files on the file list into the folder
   for (i in files[1:100]) {
     system(paste("mv", i, paste("russ-pics-", a, sep = ""), sep = " "))
   }
-  # counter + 1
-  #
-  a = a + 1
-  # loop back to beginning
   # 
-  # system call - zip all the directories in the top dir
+  # counter + 1
+  a = a + 1
+  # TODO - system call - zip all the directories in the top dir
+}
+# 
+# Dealing with the leftovers:
+system(paste("mkdir ", "russ-pics-", a, sep = ""))
+files = list.files(include.dirs = FALSE)
+for (i in files[1:length(files)]) {
+  system(paste("mv", i, paste("russ-pics-", a, sep = ""), sep = " "))
 }
